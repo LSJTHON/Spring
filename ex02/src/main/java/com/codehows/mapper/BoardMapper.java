09406ca20bfdd0a29b8b9478ce69da5d47dbbfbf
Lee Seong Jong <dltjdwhd1235@naver.com>
2024-03-13 11:56:11 +0900

    Insert 예제

--- ex02/src/main/java/com/codehows/mapper/BoardMapper.java
+++ ex02/src/main/java/com/codehows/mapper/BoardMapper.java
@@ -8,4 +8,8 @@ import com.codehows.domain.BoardVO;
 public interface BoardMapper {
 	//@Select("select * from tbl_board where bno > 0")
 	public List<BoardVO> getList();
+	
+	public void insert(BoardVO board); //pk값을 알 필요가 없는 경우
+	
+	public void insertSelectKey(BoardVO board); // pk값을 알아야 하는 경우
 }
--- ex02/src/test/java/com/codehows/mapper/BoardMapperTests.java
+++ ex02/src/test/java/com/codehows/mapper/BoardMapperTests.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import com.codehows.domain.BoardVO;
+
 import lombok.Setter;
 import lombok.extern.log4j.Log4j;
 
@@ -21,4 +23,19 @@ public class BoardMapperTests {
 	public void testGetList() {
 		mapper.getList().forEach(board -> log.info(board));
 	}
+	
+	@Test
+	public void testInsert() {
+		BoardVO board = new BoardVO();
+		board.setTitle("새로 작성하는 글");
+		board.setContent("새로 작성하는 내용");
+		board.setWriter("newbie");
+		
+		mapper.insert(board);
+		
+		log.info(board);
+	
+	}
+	
+	
 }
