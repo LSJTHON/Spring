9446729e995d28fbb7e6b8f9f010a010fe02ce07
Lee Seong Jong <dltjdwhd1235@naver.com>
2024-03-19 10:37:02 +0900

    2024-03-19 페이징 처리

--- ex03/src/test/java/com/codehows/mapper/ReplyMapperTests.java
+++ ex03/src/test/java/com/codehows/mapper/ReplyMapperTests.java
@@ -1,5 +1,6 @@
 package com.codehows.mapper;
 
+import java.util.List;
 import java.util.stream.IntStream;
 
 import org.junit.Test;
@@ -8,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.codehows.domain.Criteria;
 import com.codehows.domain.ReplyVO;
 
 import lombok.Setter;
@@ -17,27 +19,65 @@ import lombok.extern.log4j.Log4j;
 @ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
 @Log4j
 public class ReplyMapperTests {
-	private Long[] bnoArr = { 15L,13L,12L,11L,10L,9L,8L,7L,6L };
+	private Long[] bnoArr = { 13L,13L,12L,11L,10L,9L,8L,7L,6L };
 	@Setter(onMethod_ = @Autowired)
 	private ReplyMapper mapper;
 	
 	@Test
-	public void tesCreate() {
-		IntStream.rangeClosed(1,10).forEach(i -> {
-			ReplyVO vo = new ReplyVO();
-			
-			vo.setBno(bnoArr[1%5]);
-			vo.setReply("댓글 테스트"+i);
-			vo.setReplyer("replyer"+i);
-			
-			mapper.insert(vo);
-		});
+	public void testList() {
+		Criteria cri = new Criteria();
+		List<ReplyVO> replies = mapper.getListWithPaging(cri, bnoArr[0]);
+		replies.forEach(reply -> log.info(reply));
 	}
 	
-	@Test
-	public void testMapper() {
-		log.info(mapper);
-	}
+//	@Test
+//	public void testUpdate() {
+//		Long targetRno = 10L;
+//		
+//		ReplyVO vo = mapper.read(targetRno);
+//		vo.setReply("update reply");
+//		
+//		int count = mapper.update(vo);
+//		
+//		log.info("update count" + count);
+//	}
+	
+	
+//	@Test
+//	public void testDelete() {
+//		Long targetRno = 1L;
+//		
+//		mapper.delete(targetRno);
+//	}
+	
+	
+//	@Test
+//	public void testRead() {
+//		Long targetRno = 10L;
+//		
+//		ReplyVO vo = mapper.read(targetRno);
+//		
+//		log.info(vo);
+//	}
+	
+	
+//	@Test
+//	public void tesCreate() {
+//		IntStream.rangeClosed(1,10).forEach(i -> {
+//			ReplyVO vo = new ReplyVO();
+//			
+//			vo.setBno(bnoArr[1%5]);
+//			vo.setReply("댓글 테스트"+i);
+//			vo.setReplyer("replyer"+i);
+//			
+//			mapper.insert(vo);
+//		});
+//	}
+//	
+//	@Test
+//	public void testMapper() {
+//		log.info(mapper);
+//	}
 	
 	
 //	@Test
