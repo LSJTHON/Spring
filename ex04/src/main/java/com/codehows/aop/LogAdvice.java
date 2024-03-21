9e8d1f99761e78e4f13bfe8f0f78694d1a031bc4
Lee Seong Jong <dltjdwhd1235@naver.com>
2024-03-21 12:08:02 +0900

    비즈니스 계층과 트랜잭션 설정(servlet 버전 수정, OJDBC 추가)

--- ex04/src/main/java/com/codehows/aop/LogAdvice.java
+++ ex04/src/main/java/com/codehows/aop/LogAdvice.java
@@ -36,19 +36,19 @@ public class LogAdvice {
 	public Object logTime( ProceedingJoinPoint pjp) {
 		long start = System.currentTimeMillis();
 		
-		log.info("Target: "+pjp.getTarget());
-		log.info("Param: "+Arrays.toString(pjp.getArgs()));
+		log.info("Target: "+pjp.getTarget()); //메소드 객체
+		log.info("Param: "+Arrays.toString(pjp.getArgs())); //메소드 매개변수
 		
 		Object result = null;
 		try {
-			result = pjp.proceed();
+			result = pjp.proceed(); //메소드 실행 ->결과 값을 result 에 저장
 		}catch(Throwable e) {
 			e.printStackTrace();
 		}
 		
-		long end = System.currentTimeMillis();
+		long end = System.currentTimeMillis(); //메서드 실행 후 시간 값 가져오기
 		
-		log.info("TIME: " +(end- start));
+		log.info("TIME: " +(end- start)); //메소드가 실행하는데 걸린 시간 구하기
 		
 		return result;
 	}
---
+++ ex04/src/main/java/com/codehows/service/SampleTxService.java
@@ -0,0 +1,5 @@
+package com.codehows.service;
+
+public interface SampleTxService {
+	public void addData(String value);
+}
