63e6682b48c2725066124070cb4cf25a452534ec
Lee Seong Jong <dltjdwhd1235@naver.com>
2024-03-21 17:24:40 +0900

    MultipartFile 타입

--- ex05/src/main/java/com/codehows/controller/UploadController.java
+++ ex05/src/main/java/com/codehows/controller/UploadController.java
@@ -1,7 +1,12 @@
 package com.codehows.controller;
 
+import java.io.File;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.multipart.MultipartFile;
 
 import lombok.extern.log4j.Log4j;
 
@@ -12,4 +17,28 @@ public class UploadController {
 	public void uploadForm() {
 		log.info("upload form");
 	}
+	
+	@PostMapping("/uploadFormAction")
+	public void uploadFormPost(MultipartFile[] uploadFile, Model model) {
+		String uploadFolder = "C:\\upload";
+		
+		for( MultipartFile multipartFile : uploadFile) {
+			log.info("=========================");
+			log.info("Upload File Name : "+multipartFile.getOriginalFilename());
+			log.info("Upload File Size : "+multipartFile.getSize());
+			
+			File saveFile = new File(uploadFolder, multipartFile.getOriginalFilename());
+			try {
+				multipartFile.transferTo(saveFile);
+			}catch(Exception e) {
+				log.error(e.getMessage());
+			}//end catch
+		}//end for
+	}
+	
+	@GetMapping("/uploadAjax")
+	public void uploadAjax() {
+		log.info("upload Ajax");
+	}
+	
 }
